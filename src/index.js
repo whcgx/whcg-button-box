a9daf875bbbede53b5ad1982531f0338a4eb14a5
Andersh75 <andersh75@gmail.com>
2018-09-13 23:45:34 +0200

    from nm build

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
+import '@whcg/whcg-button';
+
+export class WhcgButtonBox extends PolymerElement {
+    static get template() {
+        return html `
+        <style>
+            whcg-button {
+                padding-left: 10px;
+            }
+
+        </style>
+            <whcg-button>LOGGA IN</whcg-button>
+            <whcg-button>SKAPA KONTO</whcg-button>
+    `;
+    }
+
+    static get properties() {
+
+        return {
+
+           
+        }
+    }
+
+
+
+}
+
+window.customElements.define('whcg-button-box', WhcgButtonBox);
